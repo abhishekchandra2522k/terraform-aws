a192f781ad2844bf40b86d792762e0344161b661
abhishekchandra2522k <1ac23456789@gmail.com>
2023-12-29 23:40:56 +0530

    add list-variable

--- list-variable/first.tf
+++ list-variable/first.tf
@@ -1,4 +1,17 @@
 
 output printFirst {
-    value = "first user is ${var.users[1]}"
+    # value = "first user is ${var.users[1]}"
+    value = "${join(",", var.users)}"
+}
+
+output "helloWordUpper" {
+  value = "${upper(var.users[0])}"
+}
+
+output "helloWordLower" {
+  value = "${lower(var.users[1])}"
+}
+
+output "helloWordTitle" {
+  value = "${title(var.users[2])}"
 }
