8dbd73a0a0cf2baa82a2f1c46f7e901a84c9b0a5
abhishekchandra2522k <1ac23456789@gmail.com>
2024-01-05 20:55:45 +0530

    testing terraform output

---
+++ terraform-first-resource/output.tf
@@ -0,0 +1,7 @@
+output "repourl" {
+  value = github_repository.first-automated-repo.html_url
+}
+
+output "cloneurl" {
+    value = github_repository.first-automated-repo.git_clone_url
+}
--- terraform-first-resource/terraform.tf
+++ terraform-first-resource/terraform.tf
@@ -6,12 +6,3 @@ resource "github_repository" "first-automated-repo" {
 
   auto_init = true
 }
-
-resource "github_repository" "second-automated-repo" {
-  name        = "second-automated-repo"
-  description = "Second automated repository created using Terraform"
-
-  visibility = "private"
-
-  auto_init = true
-}
