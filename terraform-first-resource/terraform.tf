4293252846093ce408704b8a9a91850afe0baf43
abhishekchandra2522k <1ac23456789@gmail.com>
2024-01-05 21:23:27 +0530

    update description

--- terraform-first-resource/terraform.tf
+++ terraform-first-resource/terraform.tf
@@ -1,6 +1,6 @@
 resource "github_repository" "first-automated-repo" {
   name        = "first-automated-repo"
-  description = "First automated repository created using Terraform"
+  description = "First automated github repository created using Terraform"
 
   visibility = "private"
 
