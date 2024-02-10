6978cb66691141ca4812712e315ee192fd49e936
abhishekchandra2522k <1ac23456789@gmail.com>
2024-02-10 21:48:34 +0530

    diversify code

--- aws-first-instance/instance.tf
+++ aws-first-instance/instance.tf
@@ -1,34 +1,3 @@
-# #creating ssh key
-resource "aws_key_pair" "key-tf" {
-  key_name   = "key-tf"
-  public_key = file("${path.module}/id_rsa.pub")
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-
-  dynamic "ingress" {
-    for_each = var.ports
-    iterator = port
-    content {
-      description = "TLS from VPC"
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]
-    }
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
 # creating ec2 instance
 resource "aws_instance" "web" {
   ami           = var.image_id
